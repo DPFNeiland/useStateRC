972cd4a22f43afb4e8698ca2e76804983baa20cb
Rodrigo Neiland <rodrigo.naohan@gmail.com>
2024-05-06 19:52:15 -0400

    BackUp060520241952

--- App.tsx
+++ App.tsx
@@ -5,10 +5,11 @@ import Atividade1 from './atividades/Atividade1/Home';
 import CampoTexto from './screens/CampoTexto/CampoTexto';
 import Atividade3 from './atividades/atividade3/Home';
 import Atividade4 from './atividades/Atividade4/atividade4';
+import Atividade5 from './atividades/Atividade5/atividade';
 
 export default function App() {
   return (
-    <Atividade4/>
+    <Atividade5/>
   );
 }
 
---
+++ atividades/Atividade5/styel.tsx
@@ -0,0 +1,62 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      backgroundColor: '#FFF'
+    },
+    
+    layout1: {
+      height: 450,
+      width: '100%',
+      flexDirection: 'column',
+      alignItems: 'center',
+      margin: 10,
+      backgroundColor: '#131047',
+      borderTopRightRadius: 30,
+      borderTopLeftRadius: 30,
+      padding: 10
+    },
+  
+    layout2: {
+        height: 200,
+        width: '100%',
+        flexDirection: 'column',
+        marginTop: 10,
+        backgroundColor: '#131047',
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+        padding: 10
+      },
+    
+        texto1:{
+        color: '#fff',
+        fontSize: 20,
+        margin: 10
+      },
+    
+        texto2:{
+        color: '#fff',
+        fontSize: 30,
+      },
+
+      input: {
+        fontSize: 15,
+        padding: 10,
+        width: 300,
+        borderRadius: 15,
+        backgroundColor: '#f1f1f1',
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        marginBottom: 20,
+      },
+    });
+
+    
+  export default styles
